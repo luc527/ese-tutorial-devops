bb0fd257f33f11ad625f48c9627a76af751b9cb0
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2023-09-03 20:14:46 -0300

    test

--- devopsnapratica/src/main/java/br/udesc/quickstart/HelloResource.java
+++ devopsnapratica/src/main/java/br/udesc/quickstart/HelloResource.java
@@ -11,6 +11,6 @@ public class HelloResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello, world!";
     }
 }
